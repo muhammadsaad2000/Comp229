5402a9fa25dee558aa74f0370d26109012fa07fa
Muhammad Saad <51520246+muhammadsaad2000@users.noreply.github.com>
2023-01-15 12:46:38 -0500

    Week 1 Files

---
+++ main.js
@@ -0,0 +1,5 @@
+//import {halfof} from './lips.js';
+//console.log(halfof(84));
+//console.log(multiply(21, 2));
+import doSomething from 'doSomething';
+doSomething();
---
+++ validator.js
@@ -0,0 +1,4 @@
+import { flag, touch } from 'validator';
+console.log(flag);
+touch();
+console.log(flag);
