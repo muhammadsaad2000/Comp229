ca099ec853e7d6126976db79fb7a6427fd1ada7e
Muhammad Saad <muhammad.saad2810@gmail.com>
2023-02-26 11:15:28 -0500

    Assignment 2

---
+++ MuhammadSaad_Assignment 2 Comp 229/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://dishanktrivedi:zwXpGS4qYv16YDdv@databaseserverassignmen.hsc7x.mongodb.net/contact_list?retryWrites=true&w=majority"
+}
